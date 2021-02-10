b2ac8ede58311f31565f4cbe0c76e455754ef918
FedericoLandaeta <65091166+FedericoLandaeta@users.noreply.github.com>
2021-02-10 12:20:32 -0500

    Add files via upload
    Initial

---
+++ RecyclingAppiOS/RecyclingAppiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecyclingAppiOS
+//
+//  Created by Vanessa Rodriguez on 2/9/21.
+//  Copyright © 2021 Federico Landaeta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
